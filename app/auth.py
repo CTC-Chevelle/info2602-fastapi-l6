ec92c53fa088a5cd4c75246e43d48e335be2f64f
CTC-Chevelle <chevelle.code.23@gmail.com>
2026-05-05 21:46:47 -0400

    Complete lab 6 - admin view, pagination, search, filtering and stats chart

--- app/auth.py
+++ app/auth.py
@@ -12,18 +12,14 @@ from jwt.exceptions import InvalidTokenError
 
 SECRET_KEY = "ThisIsAnExampleOfWhatNotToUseAsTheSecretKeyIRL"
 ALGORITHM = "HS256"
-
 password_hash = PasswordHash.recommended()
 
-# Converts plaintext password to encrypted password
-def encrypt_password(password:str):
+def encrypt_password(password: str):
     return password_hash.hash(password)
 
-# Verifies if a plaintext password, when encrypted, gives the same output as the expected encrypted password
-def verify_password(plaintext_password:str, encrypted_password):
+def verify_password(plaintext_password: str, encrypted_password):
     return password_hash.verify(password=plaintext_password, hash=encrypted_password)
 
-# This takes some information and converts it into a JWT
 def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes=15)):
     to_encode = data.copy()
     if expires_delta:
@@ -32,8 +28,7 @@ def create_access_token(data: dict, expires_delta: timedelta = timedelta(minutes
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-# Gets the current user who is making the request
-async def get_current_user(request:Request, db:SessionDep)->User:
+async def get_current_user(request: Request, db: SessionDep) -> User:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
@@ -41,24 +36,22 @@ async def get_current_user(request:Request, db:SessionDep)->User:
     )
     auth_header = request.headers.get("Authorization")
     auth_cookie = request.cookies.get("access_token")
-
     token = None
-    if auth_header and auth_header.startswith("Bearer "): # Regular auth
+    if auth_header and auth_header.startswith("Bearer "):
         token = auth_header.split(" ")[1]
-    elif auth_cookie: # Web auth
+    elif auth_cookie:
         token = auth_cookie.split(" ")[1]
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id = payload.get("sub",None)
+        user_id = payload.get("sub", None)
     except InvalidTokenError:
         raise credentials_exception
-    user = db.get(User,user_id)
-
+    user = db.get(User, user_id)
     if user is None:
         raise credentials_exception
     return user
 
-async def is_logged_in(request: Request, db:SessionDep):
+async def is_logged_in(request: Request, db: SessionDep):
     try:
         await get_current_user(request, db)
         return True
@@ -66,5 +59,17 @@ async def is_logged_in(request: Request, db:SessionDep):
         print(e)
         return False
 
+async def is_admin(user: User):
+    return user.role == "admin"
+
+async def is_admin_dep(user: Annotated[User, Depends(get_current_user)]):
+    if not await is_admin(user):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="You are not authorized to access this page",
+        )
+    return user
+
 IsUserLoggedIn = Annotated[bool, Depends(is_logged_in)]
 AuthDep = Annotated[User, Depends(get_current_user)]
+AdminDep = Annotated[User, Depends(is_admin_dep)]
---
+++ app/routers/stats.py
@@ -0,0 +1,28 @@
+from fastapi import APIRouter, Request
+from fastapi.responses import HTMLResponse
+from sqlmodel import select
+from app.database import SessionDep
+from app.models import *
+from app.auth import *
+from . import templates
+
+stats_router = APIRouter()
+
+@stats_router.get("/stats", response_class=HTMLResponse)
+async def stats_page(request: Request, user: AdminDep):
+    return templates.TemplateResponse(
+        request=request,
+        name="stats.html",
+        context={"current_user": user}
+    )
+
+@stats_router.get("/todo-stats")
+async def todo_stats(request: Request, user: AdminDep, db: SessionDep):
+    todos = db.exec(select(Todo)).all()
+    res = {}
+    for todo in todos:
+        if todo.user.username in res:
+            res[todo.user.username] += 1
+        else:
+            res[todo.user.username] = 1
+    return res
